579a9630ff096f01eebfd24efeb06ddd87936dbc
Krenar <krenarmuzaqi@gmail.com>
2020-04-29 13:41:24 +0200

    Widget modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-import Header from "./components/Header";
-import Balance from "./components/Balance";
-import IncomeExpenses from "./components/IncomeExpenses";
-import TransactionList from "./components/TransactionList";
-import AddTransaction from "./components/AddTransaction";
+import Header from "./Header";
+import Balance from "./Balance";
+import IncomeExpenses from "./IncomeExpenses";
+import TransactionList from "./TransactionList";
+import AddTransaction from "./AddTransaction";
 
-import { GlobalProvider } from "./context/GlobalState";
+import { GlobalProvider } from "../context/GlobalState";
 
-import "./app.css";
+import "../app.css";
 
 const App = () => {
   return (
